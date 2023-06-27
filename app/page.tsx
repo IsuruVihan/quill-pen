73a9574746775654b4e04b9b2adf58eec7a016a0
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2023-06-27 10:00:04 +0530

    Removed workflows

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1>Home page!</h1>
       <br/>
       <h2>Testing the GitHub actions</h2>
-      <h2>Testing the GitHub actions </h2>
+      <h2>Testing the GitHub actions 2</h2>
     </main>
   );
 }
