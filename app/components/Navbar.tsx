d4bb4db262970332c36fc901fea034a00304575a
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2023-06-29 11:24:43 +0530

    Added Changed responsive styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,12 +5,13 @@ import QuillPenIcon from "@/public/icon.png";
 
 export default function Navbar() {
   return (
-    <nav className="w-full h-75px px-5 lg:px-44 border-solid border-b-1 border-black bg-amber-400 flex flex-row justify-between align-middle">
+    <nav className="fixed w-full h-75px px-5 md:px-12 lg:px-16 2xl:px-44 border-solid border-b-1 border-black
+    bg-amber-400 flex flex-row justify-between align-middle">
       <Link href="/" className="flex items-center">
         <Image src={QuillPenIcon} height={30} alt="WriteIt Logo"/>
-        <p className="pl-1 text-3xl text-zinc-800 font-bold font-serif">QuillPen</p>
+        <p className="pl-1 text-3xl text-zinc-800 font-bold font-PTSerif">QuillPen</p>
       </Link>
-      <div className="w-auto sm:w-40 md:w-26rem flex justify-between items-center text-sm font-medium">
+      <div className="w-auto sm:w-40 md:w-26rem flex justify-between items-center text-sm font-semibold">
         <Link href="/" className="hidden md:block">Our story</Link>
         <Link href="/" className="hidden md:block">Membership</Link>
         <Link href="/" className="hidden md:block">Write</Link>
